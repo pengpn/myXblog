31b28c0b78374aaa175626117849b030a966492d
pengpn <312924860@qq.com>
2017-05-25 17:53:21 +0800

    add blog route

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//User Auth
+Auth::routes();
 
 // Site route //指定路由名称到控制器动作
 Route::get('/',['uses' => 'HomeController@index', 'as' => 'index']);
 
-Auth::routes();
 
+//Post
+Route::get('/blog',['uses' => 'PostController@index', 'as' => 'post.index']);
 

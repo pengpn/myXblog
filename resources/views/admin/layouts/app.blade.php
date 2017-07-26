8c6546e53e611ba51cc224da44d4e1d75be0001f
pengpn <312924860@qq.com>
2017-07-26 13:20:56 +0800

    add admin layouts and index

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="Zh_cn" xmlns:v-on="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="{{ $author or '' }}">
+    <title>@yield('title') {{ $site_title or '' }} </title>
+    <meta name="keywords" content="@yield('keywords') {{ $site_keywords or '' }}">
+    <meta name="description" content="@yield('description') {{ $site_description or '' }}">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="{{ $site_title or '' }}">
+    <meta property="og:site_name" content="{{ $site_title or '' }}">
+    <meta property="og:description" content="{{ $site_description or '' }}">
+    <meta name="theme-color" content="#52768e">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link href="//cdn.bootcss.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Styles -->
+    @if(isset($site_css) && $site_css)
+        <link rel="stylesheet" href="{{ $site_css }}">
+    @else
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+@endif
+@yield('css')
+<!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+</head>
+<body>
+@include('admin.layouts.header')
+<div id="content-wrap">
+    <div class="container">
+        @include('partials.errors')
+        @include('partials.success')
+        @yield('content')
+    </div>
+</div>
+@include('layouts.footer')
+@include('layouts.footer')
+@if(isset($site_js) && $site_js)
+    <script src="{{ $site_js }}"></script>
+@else
+    <script src="{{ mix('js/app.js') }}"></script>
+@endif
+@yield('script')
+
+</body>
+</html>
